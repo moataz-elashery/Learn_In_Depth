2d08edcf72ee3421aa9a2a39c1cdcea2d798fcf0
Moataz Elashery <74562285+moataz-elashery@users.noreply.github.com>
2022-10-18 15:28:19 +0200

    HW6 Q4 Done: Print array in reverse using pointers

---
+++ Unit2_C_Programming/unit2_lesson8_pointers/hw6_q4_print_array_in_reverse.c
@@ -0,0 +1,41 @@
+/*
+ * hw6_q4_print_array_in_reverse.c
+ *
+ *  Created on: Oct 18, 2022 at 3:19:38 PM
+ *      Author: Moataz Elashery
+ */
+
+
+#include <stdio.h>
+
+#define size 5
+
+int main()
+{
+	printf("Input 5 number of elements in the array: \n");
+	int input[size];
+
+	int i;
+	for(i=0;i<size;i++)
+	{
+		printf("element - %d: ",i+1);
+		fflush(stdin);	fflush(stdout);
+		scanf("%d",&input[i]);
+
+	}
+
+	int *pint=&input[0];
+	printf("The elements of array in reverse order are :\n");
+
+	for(i=size-1;i>=0;i--)
+	{
+		printf("element - %d: %d ",i+1,*(pint+i));
+
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+
